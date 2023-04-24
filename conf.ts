3b898925ffdb7dc5576b7cd059c356f74744217e
alex65cast <alex65578@gmail.com>
2023-04-24 12:56:00 +0200

    Variables de entorno actualizada2

--- conf.ts
+++ conf.ts
@@ -2,7 +2,7 @@ const CONF = {
     JWT_SECRET: process.env.JWT_SECRET,
     SALT_ROUND: Number(process.env.SALT_ROUND),
     ALLOW_OBOARDING: Boolean(process.env.ALLOW_OBOARDING),
-    CONECT_DB: process.env.CONECT_DB 
+    CONECT_DB: process.env.CONECT_DB!
 }
 
 export default CONF;
